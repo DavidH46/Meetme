795bd1f89273064bc6225c8ec5a78bdfd8251991
David <d5561633@gmail.com>
2016-01-30 01:25:30 +0800

    code

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layout')
+
+@section('content')
+<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+<div class="form-group">
+  <div class="togglebutton">
+    <label>
+      <input type="checkbox" checked> Toggle button
+    </label>
+  </div>
+</div>
+@endsection
---
+++ resources/views/test2.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.layout')
+
+@section('content')
+<div id="example"></div>
+<script type="text/babel" src="js/helloworld.js"></script>
+@endsection
